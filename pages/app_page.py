41580b8973c658ed5e31957c5a721ea159f5d84d
Richard Qian <richardqiancs@gmail.com>
2023-04-11 17:59:49 -0400

    Trying to graph some stuff

--- pages/app_page.py
+++ pages/app_page.py
@@ -2,34 +2,44 @@ import dash
 import cx_Oracle
 import pandas as pd
 import dash_bootstrap_components as dbc
+import plotly.express as px
 
-from dash import html
+from dash import html, dcc
 from dash import dash_table
 from components.navbar import navbar
 from components.footer import footer
 
-# These two lines of code are needed for the Oracle client to work on my Windows machine. If you are on windows,
-    # replace the path with the path to your Oracle Instant Client (the path you added as an environment variable).
-    # Otherwise, comment it out and disregard.
-path_of_oracle_instant_client = r"D:\Program Files\Oracle\instantclient_21_9"
-cx_Oracle.init_oracle_client(lib_dir=path_of_oracle_instant_client)
+def query_db(sql_query):
+    # These two lines of code are needed for the Oracle client to work on my Windows machine. If you are on windows,
+        # replace the path with the path to your Oracle Instant Client (the path you added as an environment variable).
+        # Otherwise, comment it out and disregard.
+    path_of_oracle_instant_client = r"D:\Program Files\Oracle\instantclient_21_9"
+    cx_Oracle.init_oracle_client(lib_dir=path_of_oracle_instant_client)
 
-# Connect to Oracle Database
-conn = cx_Oracle.connect(user='williamsobczak', password='REBY7TpizLTdOp5dZHa9qJS0',
-                         dsn=cx_Oracle.makedsn('oracle.cise.ufl.edu', '1521',
-                                              sid='orcl'))
-cursor = conn.cursor()
-sql_query = 'SELECT * FROM adultmortality FETCH FIRST 20 ROWS ONLY'
-cursor.execute(sql_query)
-results = cursor.fetchall()
+    # Connect to Oracle Database
+    conn = cx_Oracle.connect(user='williamsobczak', password='REBY7TpizLTdOp5dZHa9qJS0',
+                            dsn=cx_Oracle.makedsn('oracle.cise.ufl.edu', '1521',
+                                                sid='orcl'))
+    cursor = conn.cursor()
+    cursor.execute(sql_query)
+    results = cursor.fetchall()
 
-df = pd.DataFrame(results, columns=[desc[0] for desc in cursor.description])
-cursor.close()
-conn.close()
+    df = pd.DataFrame(results, columns=[desc[0] for desc in cursor.description])
+    cursor.close()
+    conn.close()
+
+    return df
+
+def reformat_data_label(label):
+    words = label.split('_')
+    formatted_words = [word.lower().capitalize() for word in words]
+    return ' '.join(formatted_words)
 
 
 nav = navbar()
 ftr = footer()
+
+df = query_db('SELECT * FROM adultmortality FETCH FIRST 20 ROWS ONLY')
 
 body = dbc.Container(
     [
@@ -50,7 +60,7 @@ body = dbc.Container(
                                 style_table={'width': '50%'},
                                 style_cell={'textAlign': 'left', 'fontSize': 14}
                             )
-                        ])
+                        ]),
                     ],
                     md=4,
                 )
@@ -60,7 +70,32 @@ body = dbc.Container(
     className="mt-4 body-flex-wrapper",
 )
 
+fig = px.line(df, x="YEAR", y="BOTH_SEXES_MORTALITY_RATE", title='Adult Mortality Rate', color = 'COUNTRY')
+fig.update_xaxes(title_text=(reformat_data_label("YEAR")))
+fig.update_yaxes(title_text=(reformat_data_label("BOTH_SEXES_MORTALITY_RATE")))
+
+data_visualization = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.Div(
+                            [
+                                dcc.Graph(
+                                    id='data-visualization',
+                                    figure=fig
+                                )
+                            ]
+                        )
+                    ]
+                )
+            ]
+        )
+    ]
+)
+
 
 def app_page(app: dash.Dash):
-    layout = html.Div([nav, body, ftr], className="make-footer-stick")
+    layout = html.Div([nav, body, data_visualization, ftr], className="make-footer-stick")
     return layout
