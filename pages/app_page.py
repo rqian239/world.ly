ccbd1bfa7254badd9a92ebdb49aede2de2dc5eb9
Nikhil Pandya <nikhil.pandya@ufl.edu>
2023-04-13 17:19:36 -0400

    scatter plot added

--- pages/app_page.py
+++ pages/app_page.py
@@ -4,6 +4,8 @@ import pandas as pd
 import dash_bootstrap_components as dbc
 import plotly.express as px
 
+import plotly.graph_objs as go
+
 from dash import html, dcc
 from dash import dash_table
 from components.navbar import navbar
@@ -13,7 +15,7 @@ def query_db(sql_query):
     # These two lines of code are needed for the Oracle client to work on my Windows machine. If you are on windows,
         # replace the path with the path to your Oracle Instant Client (the path you added as an environment variable).
         # Otherwise, comment it out and disregard.
-    path_of_oracle_instant_client = r"D:\Program Files\Oracle\instantclient_21_9"
+    path_of_oracle_instant_client = r"C:\Program Files\Oracle\instantclient_21_9"
     cx_Oracle.init_oracle_client(lib_dir=path_of_oracle_instant_client)
 
     # Connect to Oracle Database
@@ -39,8 +41,8 @@ def reformat_data_label(label):
 nav = navbar()
 ftr = footer()
 
-query_string = '(SELECT year, life_expectancy_change_compared_to_last_5_years FROM ( SELECT year, ROUND(life_expectancy - past_5_yrs_avg_life_expectancy,4) life_expectancy_change_compared_to_last_5_years FROM ( SELECT year, life_expectancy, AVG(life_expectancy) OVER(ORDER BY year DESC ROWS BETWEEN 1 FOLLOWING AND 5 FOLLOWING) past_5_yrs_avg_life_expectancy FROM ( SELECT year, AVG(life_expectancy_at_birth) life_expectancy FROM LifeExpectancy GROUP BY year ORDER BY YEAR DESC ) ) WHERE YEAR > 1950 AND YEAR < 2020 ) ORDER BY life_expectancy_change_compared_to_last_5_years FETCH FIRST 5 ROWS ONLY) UNION ALL (SELECT year, life_expectancy_change_compared_to_last_5_years FROM (SELECT year, ROUND(life_expectancy - past_5_yrs_avg_life_expectancy,4) life_expectancy_change_compared_to_last_5_years FROM (SELECT year, life_expectancy, AVG(life_expectancy) OVER(ORDER BY year DESC ROWS BETWEEN 1 FOLLOWING AND 5 FOLLOWING) past_5_yrs_avg_life_expectancy FROM (SELECT year, AVG(life_expectancy_at_birth) life_expectancy FROM LifeExpectancy GROUP BY year ORDER BY YEAR DESC)) WHERE YEAR > 1950 AND YEAR < 2020) ORDER BY life_expectancy_change_compared_to_last_5_years DESC FETCH FIRST 5 ROWS ONLY)'
-
+#query_string = '(SELECT year, life_expectancy_change_compared_to_last_5_years FROM ( SELECT year, ROUND(life_expectancy - past_5_yrs_avg_life_expectancy,4) life_expectancy_change_compared_to_last_5_years FROM ( SELECT year, life_expectancy, AVG(life_expectancy) OVER(ORDER BY year DESC ROWS BETWEEN 1 FOLLOWING AND 5 FOLLOWING) past_5_yrs_avg_life_expectancy FROM ( SELECT year, AVG(life_expectancy_at_birth) life_expectancy FROM LifeExpectancy GROUP BY year ORDER BY YEAR DESC ) ) WHERE YEAR > 1950 AND YEAR < 2020 ) ORDER BY life_expectancy_change_compared_to_last_5_years FETCH FIRST 5 ROWS ONLY) UNION ALL (SELECT year, life_expectancy_change_compared_to_last_5_years FROM (SELECT year, ROUND(life_expectancy - past_5_yrs_avg_life_expectancy,4) life_expectancy_change_compared_to_last_5_years FROM (SELECT year, life_expectancy, AVG(life_expectancy) OVER(ORDER BY year DESC ROWS BETWEEN 1 FOLLOWING AND 5 FOLLOWING) past_5_yrs_avg_life_expectancy FROM (SELECT year, AVG(life_expectancy_at_birth) life_expectancy FROM LifeExpectancy GROUP BY year ORDER BY YEAR DESC)) WHERE YEAR > 1950 AND YEAR < 2020) ORDER BY life_expectancy_change_compared_to_last_5_years DESC FETCH FIRST 5 ROWS ONLY)'
+query_string = '(SELECT education.entity, education.percentage percentage_with_tertiary_education, income.value per_capita_income FROM ShareOfThePopulationWithCompletedTertiaryEducation education, GrossNationalIncomePerCapita income WHERE education.code = income.code AND education.year = income.year AND education.year = 2010)'
 df = query_db(query_string)
 
 body = dbc.Container(
@@ -72,10 +74,6 @@ body = dbc.Container(
     className="mt-4 body-flex-wrapper",
 )
 
-# fig = px.line(df, x="YEAR", y="BOTH_SEXES_MORTALITY_RATE", title='Adult Mortality Rate', color = 'COUNTRY')
-# fig.update_xaxes(title_text=(reformat_data_label("YEAR")))
-# fig.update_yaxes(title_text=(reformat_data_label("BOTH_SEXES_MORTALITY_RATE")))
-
 data_visualization = dbc.Container(
     [
         dbc.Row(
@@ -84,10 +82,24 @@ data_visualization = dbc.Container(
                     [
                         html.Div(
                             [
-                                # dcc.Graph(
-                                #     id='data-visualization',
-                                #     figure=fig
-                                # )
+                                dcc.Graph(
+                                    id='scatter-plot',
+                                    figure={
+                                        'data': [
+                                            go.Scatter(
+                                                x=df['PERCENTAGE_WITH_TERTIARY_EDUCATION'],
+                                                y=df['PER_CAPITA_INCOME'],
+                                                mode='markers',
+                                                text=df['ENTITY']
+                                            )
+                                        ],
+                                        'layout': go.Layout(
+                                            title='Sample Scatter Plot',
+                                            xaxis={'title': 'PERCENTAGE_WITH_TERTIARY_EDUCATION'},
+                                            yaxis={'title': 'PER_CAPITA_INCOME'}
+                                        )
+                                    }
+                                )
                             ]
                         )
                     ]
@@ -96,6 +108,60 @@ data_visualization = dbc.Container(
         )
     ]
 )
+
+# body = dbc.Container(
+#     [
+#         dbc.Row(
+#             [
+#                 dbc.Col(
+#                     [
+#                         html.H1("Main Application"),
+#                         html.P(
+#                             """\
+#                             This is where we put the data visualizations."""
+#                         ),
+#                         html.Div([
+#                             dash_table.DataTable(
+#                                 id='table',
+#                                 columns=[{"name": i, "id": i} for i in df.columns],
+#                                 data=df.to_dict('records'),
+#                                 style_table={'width': '50%'},
+#                                 style_cell={'textAlign': 'left', 'fontSize': 14}
+#                             )
+#                         ]),
+#                     ],
+#                     md=4,
+#                 )
+#             ]
+#         )
+#     ],
+#     className="mt-4 body-flex-wrapper",
+# )
+
+# fig = px.line(df, x="YEAR", y="BOTH_SEXES_MORTALITY_RATE", title='Adult Mortality Rate', color = 'COUNTRY')
+# fig.update_xaxes(title_text=(reformat_data_label("YEAR")))
+# fig.update_yaxes(title_text=(reformat_data_label("BOTH_SEXES_MORTALITY_RATE")))
+
+# data_visualization = dbc.Container(
+#     [
+#         dbc.Row(
+#             [
+#                 dbc.Col(
+#                     [
+#                         html.Div(
+#                             [
+#                                 # dcc.Graph(
+#                                 #     id='data-visualization',
+#                                 #     figure=fig
+#                                 # )
+#                             ]
+#                         )
+#                     ]
+#                 )
+#             ]
+#         )
+#     ]
+# )
 
 
 def app_page(app: dash.Dash):
