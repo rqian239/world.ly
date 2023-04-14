c4430c302d9e93c6fd1f9a79274d1bd428a2cb85
Richard Qian <richardqiancs@gmail.com>
2023-04-13 22:28:21 -0400

    Add scatterplot and world map on the same page

--- pages/app_page.py
+++ pages/app_page.py
@@ -17,7 +17,7 @@ nav = navbar()
 ftr = footer()
 
 #query_string = '(SELECT year, life_expectancy_change_compared_to_last_5_years FROM ( SELECT year, ROUND(life_expectancy - past_5_yrs_avg_life_expectancy,4) life_expectancy_change_compared_to_last_5_years FROM ( SELECT year, life_expectancy, AVG(life_expectancy) OVER(ORDER BY year DESC ROWS BETWEEN 1 FOLLOWING AND 5 FOLLOWING) past_5_yrs_avg_life_expectancy FROM ( SELECT year, AVG(life_expectancy_at_birth) life_expectancy FROM LifeExpectancy GROUP BY year ORDER BY YEAR DESC ) ) WHERE YEAR > 1950 AND YEAR < 2020 ) ORDER BY life_expectancy_change_compared_to_last_5_years FETCH FIRST 5 ROWS ONLY) UNION ALL (SELECT year, life_expectancy_change_compared_to_last_5_years FROM (SELECT year, ROUND(life_expectancy - past_5_yrs_avg_life_expectancy,4) life_expectancy_change_compared_to_last_5_years FROM (SELECT year, life_expectancy, AVG(life_expectancy) OVER(ORDER BY year DESC ROWS BETWEEN 1 FOLLOWING AND 5 FOLLOWING) past_5_yrs_avg_life_expectancy FROM (SELECT year, AVG(life_expectancy_at_birth) life_expectancy FROM LifeExpectancy GROUP BY year ORDER BY YEAR DESC)) WHERE YEAR > 1950 AND YEAR < 2020) ORDER BY life_expectancy_change_compared_to_last_5_years DESC FETCH FIRST 5 ROWS ONLY)'
-query_string = '(SELECT education.code, education.entity, education.percentage percentage_with_tertiary_education FROM ShareOfThePopulationWithCompletedTertiaryEducation education WHERE education.year = 2010)'
+query_string = 'SELECT education.code, education.entity, education.percentage percentage_with_tertiary_education, income.value per_capita_income FROM ShareOfThePopulationWithCompletedTertiaryEducation education, GrossNationalIncomePerCapita income WHERE education.code = income.code AND education.year = income.year AND education.year = \'2010\' '
 df = functions.query_db(query_string)
 
 body = dbc.Container(
@@ -49,63 +49,22 @@ body = dbc.Container(
     className="mt-4 body-flex-wrapper",
 )
 
-# data_visualization = dbc.Container(
-#     [
-#         dbc.Row(
-#             [
-#                 dbc.Col(
-#                     [
-#                         html.Div(
-#                             [
-#                                 dcc.Graph(
-#                                     id='scatter-plot',
-#                                     figure={
-#                                         'data': [
-#                                             go.Scatter(
-#                                                 x=df['PERCENTAGE_WITH_TERTIARY_EDUCATION'],
-#                                                 y=df['PER_CAPITA_INCOME'],
-#                                                 mode='markers',
-#                                                 text=df['ENTITY']
-#                                             )
-#                                         ],
-#                                         'layout': go.Layout(
-#                                             title='Sample Scatter Plot',
-#                                             xaxis={'title': 'PERCENTAGE_WITH_TERTIARY_EDUCATION'},
-#                                             yaxis={'title': 'PER_CAPITA_INCOME'}
-#                                         )
-#                                     }
-#                                 )
-#                             ]
-#                         )
-#                     ]
-#                 )
-#             ]
-#         )
-#     ]
-# )
-
 # fig = px.line(df, x="YEAR", y="BOTH_SEXES_MORTALITY_RATE", title='Adult Mortality Rate', color = 'COUNTRY')
 # fig.update_xaxes(title_text=(reformat_data_label("YEAR")))
 # fig.update_yaxes(title_text=(reformat_data_label("BOTH_SEXES_MORTALITY_RATE")))
 
-# fig = px.scatter(df, x='PERCENTAGE_WITH_TERTIARY_EDUCATION', y='PER_CAPITA_INCOME', hover_name='ENTITY', color='ENTITY')
-# fig.update_xaxes(title_text=(functions.reformat_data_label("PERCENTAGE_WITH_TERTIARY_EDUCATION")))
-# fig.update_yaxes(title_text=(functions.reformat_data_label("PER_CAPITA_INCOME")))
+scatter_fig = px.scatter(df, x='PERCENTAGE_WITH_TERTIARY_EDUCATION', y='PER_CAPITA_INCOME', hover_name='ENTITY', color='ENTITY')
+scatter_fig.update_xaxes(title_text=(functions.reformat_data_label("PERCENTAGE_WITH_TERTIARY_EDUCATION")))
+scatter_fig.update_yaxes(title_text=(functions.reformat_data_label("PER_CAPITA_INCOME")))
+scatter_fig.update_layout(width=1500, height=1000)
 
-# fig.update_layout(width=1500, height=1000)
-
-test = px.data.gapminder().query("year==2007")
-print(test.head(10))
-
-fig = px.scatter_geo(df, locations = 'CODE',
-                     color='ENTITY',
-                     hover_name='ENTITY',
-                     size='PERCENTAGE_WITH_TERTIARY_EDUCATION',
-                     projection='orthographic')
-fig.update_layout(width=1500, height=1000)
-
-data_visualization = dbc.Container(
+scatter_plot_visualization = dbc.Container(
     [
+    # Add dropdown and buttons here
+        dbc.Row(
+            [
+            ]
+        ),
         dbc.Row(
             [
                 dbc.Col(
@@ -114,7 +73,41 @@ data_visualization = dbc.Container(
                             [
                                 dcc.Graph(
                                     id='data-visualization',
-                                    figure=fig
+                                    figure=scatter_fig
+                                ),
+                            ],
+                            # className="center-content"
+                        )
+                    ]
+                )
+            ]
+        ),
+    ]
+)
+
+world_map_fig = px.scatter_geo(df, locations = 'CODE',
+                     color='ENTITY',
+                     hover_name='ENTITY',
+                     size='PERCENTAGE_WITH_TERTIARY_EDUCATION',
+                     projection='orthographic')
+world_map_fig.update_layout(width=1500, height=1000)
+
+world_map_visualization = dbc.Container(
+    [
+    # Add dropdown and buttons here
+        dbc.Row(
+            [
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.Div(
+                            [
+                                dcc.Graph(
+                                    id='data-visualization',
+                                    figure=world_map_fig
                                 ),
                             ],
                             # className="center-content"
@@ -129,5 +122,5 @@ data_visualization = dbc.Container(
 
 
 def app_page(app: dash.Dash):
-    layout = html.Div([nav, body, data_visualization, ftr], className="make-footer-stick")
+    layout = html.Div([nav, body, scatter_plot_visualization, world_map_visualization, ftr], className="make-footer-stick")
     return layout
