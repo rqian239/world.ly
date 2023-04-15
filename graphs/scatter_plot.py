45dd3597343d3349d0ee6bcb4b9644963b1fb619
NP <nikhil.pandya@ufl.edu>
2023-04-15 00:36:53 -0400

    s tier globe

--- graphs/scatter_plot.py
+++ graphs/scatter_plot.py
@@ -2,6 +2,7 @@ import dash
 import pandas as pd
 import dash_bootstrap_components as dbc
 import plotly.express as px
+import numpy as np
 
 import plotly.graph_objs as go
 
@@ -18,9 +19,17 @@ import data
 query_string = 'SELECT education.code, education.entity, education.year, education.percentage_with_tertiary_education percentage_with_tertiary_education, income.gross_national_income_per_capita per_capita_income FROM ShareOfThePopulationWithCompletedTertiaryEducation education, GrossNationalIncomePerCapita income WHERE education.code = income.code AND education.year = income.year AND education.year = \'2010\' '
 df = functions.query_db(query_string)
 
+# x='PERCENTAGE_WITH_TERTIARY_EDUCATION'
+# y='PER_CAPITA_INCOME'
+# coefficients = np.polyfit(x, y, 1)
+# line = coefficients[0] * x + coefficients[1]
+
 scatter_fig = px.scatter(df, x='PERCENTAGE_WITH_TERTIARY_EDUCATION', y='PER_CAPITA_INCOME', hover_name='ENTITY', color='ENTITY')
 scatter_fig.update_xaxes(title_text=(functions.reformat_data_label("PERCENTAGE_WITH_TERTIARY_EDUCATION")))
 scatter_fig.update_yaxes(title_text=(functions.reformat_data_label("PER_CAPITA_INCOME")))
+
+# scatter_fig.add_trace(px.line(x=x, y=line).data[0])
+
 scatter_fig.update_layout(width=1300, height=1000)
 
 scatter_plot_section = dbc.Container(
